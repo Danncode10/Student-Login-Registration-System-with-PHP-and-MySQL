ec067796c5009092e4f17370d96c9d71106d9667
Dann <Lesterdannlopez7@gmail.com>
2025-05-21 22:36:43 +0800

    Updated the Link in Home page Github

--- home.php
+++ home.php
@@ -50,7 +50,7 @@ $username = htmlspecialchars($_SESSION['username']);
                                 <h1 class="display-3 fw-bolder mb-5"><span class="text-gradient d-inline">Explore My Information Management Project</span></h1>
                                 <div class="d-grid gap-3 d-sm-flex justify-content-sm-center justify-content-xxl-start mb-3">
                                     <a class="btn btn-primary btn-lg px-5 py-3 me-sm-3 fs-6 fw-bolder" href="logout.php">Log out</a>
-                                    <a class="btn btn-outline-dark btn-lg px-5 py-3 fs-6 fw-bolder" href="https://github.com/Danncode10">Github</a>
+                                    <a class="btn btn-outline-dark btn-lg px-5 py-3 fs-6 fw-bolder" href="https://github.com/Danncode10/Student-Login-Registration-System-with-PHP-and-MySQL">Github</a>
                                 </div>
                             </div>
                         </div>
